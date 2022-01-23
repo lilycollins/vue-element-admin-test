dd02b26691ee5e5ad9cf9289c92ad18a67ae37d9
zuoakanag <zuoakang@gmail.com>
2022-01-23 19:59:26 +0800

    add page

--- src/views/login/auth-redirect.vue
+++ src/views/login/auth-redirect.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="element-main">
-    <el-button style="margin: 12px" size="small" type="plain" @click="back">返回登录</el-button>
-    <div style="width: 90%;margin: 20px auto">
-      <h3>忘记密码</h3>
-      <el-row style="width: 80%">
-        <el-steps :active="active" finish-status="success">
-          <el-step title="第1步" />
-          <el-step title="第2步" />
-          <el-step title="已完成" />
+  <div class="forget-password">
+    <el-button
+      style="margin: 12px"
+      size="small"
+      type="plain"
+      @click="back"
+    >返回登录</el-button>
+    <div style="width: 70%; margin: 20px auto">
+      <h2>忘记密码</h2>
+      <el-row style="width: 100%; padding-top: 55px">
+        <el-steps :active="active" style="padding: 55x 0">
+          <el-step title="第1步" icon="el-icon-success" />
+          <el-step title="第2步" icon="el-icon-success" />
+          <el-step title="已完成" icon="el-icon-success" />
         </el-steps>
         <el-button type="primary" @click="next">下一步</el-button>
       </el-row>
-
     </div>
   </div>
 </template>
@@ -36,6 +40,6 @@ export default {
 
 <style lang="scss" scoped>
 .m50 {
-    margin: 50px;
+  margin: 50px;
 }
 </style>
