3512fd65033fe22a3cbe92526c0a10494adcb181
zuoak <zuoak@jiguang.cn>
2022-01-24 22:20:26 +0800

    add cost info

---
+++ src/views/consume/consumeInfo.vue
@@ -0,0 +1,302 @@
+<template>
+  <div class="main-content">
+    <div class="sub-title-head">
+      <div class="l-box" />
+      耗材信息
+    </div>
+    <div style="display: flex; justify-content: left">
+      <div class="tag-warp">
+        <el-button plain style="background: gold">刷新</el-button>
+        <el-button type="primary" @click="addIt">创建</el-button>
+        <el-button
+          type="danger"
+          :disabled="selected.length == 0"
+          @click="deleteIt"
+        >删除
+        </el-button>
+      </div>
+      <div style="margin-left: 10px;">
+        <el-dropdown>
+          <el-button type="info" style="height: 41px;">
+            更多菜单<i class="el-icon-arrow-down el-icon--right" />
+          </el-button>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+            <el-dropdown-item>双皮奶</el-dropdown-item>
+            <el-dropdown-item>蚵仔煎</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+      <el-form :inline="true" :model="formInline" class="demo-form-inline" style="margin-left: 10px;">
+        <el-form-item label="">
+          <el-input
+            v-model="formInline.user"
+            prefix-icon="el-icon-search"
+            placeholder="搜索供应商编号/名称"
+          />
+        </el-form-item>
+        <el-form-item style="margin-left: 10px;">
+          <el-button plain class="search-btn" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+
+    <div style="display: flex;justify-content: space-between">
+      <div style="flex: 1;padding: 20px;">
+        <div style="height: 30px;background: #cccccc;text-align: center;line-height: 30px;">
+          耗材类型
+        </div>
+        <div>
+          <el-row class="tac">
+            <el-col>
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+              >
+                <el-submenu index="1">
+                  <template slot="title">
+                    <div style="padding-left: 20px !important;">
+                      <i class="el-icon-location" />
+                      <span>导航一</span>
+                    </div>
+
+                  </template>
+                  <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu" />
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <i class="el-icon-document" />
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <i class="el-icon-setting" />
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div style="flex: 2;padding: 20px;">
+        <div style="height: 30px;background: #cccccc;text-align: center;line-height: 30px;">
+          商品类型
+        </div>
+        <!-- 表格 -->
+        <my-table :tb="tb" @choose="tbSelect" @editRow="editRow" />
+        <!-- 分页 -->
+        <pagination
+          v-show="total >= 0"
+          :total="total"
+          :page.sync="form.pageIndex"
+          :limit.sync="form.pageSize"
+          @pagination="getList"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import myTable from '@/components/Table'
+import pagination from '@/components/Pagination'
+
+const field = [
+  {
+    id: 2,
+    type: 0,
+    label: '图片',
+    key: 'a1'
+  },
+  {
+    id: 3,
+    type: 0,
+    label: '耗材编号',
+    key: 'a2',
+    showOverflow: true
+  },
+  {
+    id: 4,
+    type: 0,
+    label: '耗材名称',
+    key: 'a3'
+  }, {
+    id: 5,
+    type: 0,
+    label: '耗材类型',
+    key: 'a4'
+  }, {
+    id: 6,
+    type: 0,
+    label: '单位',
+    key: 'a5'
+  }, {
+    id: 7,
+    type: 0,
+    label: '供应商',
+    key: 'a6'
+  }, {
+    id: 8,
+    type: 0,
+    label: '保质期',
+    key: 'a7'
+  }, {
+    id: 10,
+    type: 0,
+    label: '创建日期',
+    key: 'a8'
+  }]
+export default {
+  name: 'ConsumeInfo',
+  components: {
+    myTable,
+    pagination
+  },
+  data() {
+    return {
+      form: {
+        pageIndex: 1,
+        pageSize: 20
+      },
+      formInline: {
+        user: '',
+        time: ''
+      },
+      tabPosition: 'check',
+      tb: {
+        fields: field,
+        data: [],
+        index: true,
+        selectionBtn: true,
+        height: null,
+        operationName: '操作',
+        operationWidth: '180',
+        OperationBtn: [
+          {
+            type: 'text',
+            key: '1',
+            name: '编辑'
+          }
+        ]
+      },
+      total: 0,
+      selected: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  mounted() {
+    this.tb.height = document.body.clientHeight - 240
+  },
+  methods: {
+    getList() {
+      this.tb.data = [{
+        'a1': '0001',
+        'a2': '1234567890123',
+        'a3': '得力A4 70g打印纸',
+        'a4': '办公用品',
+        'a5': '包',
+        'a6': '米宝供应商',
+        'a7': '12个月',
+        'a8': '2021-01-02  12:12'
+      }, {
+        'a1': '0002',
+        'a2': '1234567890123',
+        'a3': '得力A4 70g打印纸',
+        'a4': '办公用品',
+        'a5': '包',
+        'a6': '米宝供应商',
+        'a7': '12个月',
+        'a8': '2021-01-02  12:12'
+      }, {
+        'a1': '0003',
+        'a2': '1234567890123',
+        'a3': '得力A4 70g打印纸',
+        'a4': '办公用品',
+        'a5': '包',
+        'a6': '米宝供应商',
+        'a7': '12个月',
+        'a8': '2021-01-02  12:12'
+      }]
+      this.total = 3
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleSizeChange(val) {
+      this.form.pageSize = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.form.pageIndex = val
+      this.getList()
+    },
+    onSubmit() {
+      this.form.pageIndex = 1
+      this.getList()
+    },
+    tbSelect(select) {
+      this.selected = []
+      select.forEach(ele => {
+        this.selected.push(ele.id)
+      })
+    },
+    editRow({ row }, btn, index) {
+      this.$router.push({
+        name: 'supplyChange', params: {
+          type: 'edit'
+        }
+      })
+    },
+    addIt() {
+      this.$router.push({
+        name: 'supplyChange', params: {
+          type: 'add'
+        }
+      })
+    },
+    deleteIt() {
+      this.$alert('确定要删除吗？', '确认提示', {
+        confirmButtonText: '确定'
+      }).then(() => {
+        this.$message.success('操作成功')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-dropdown {
+  vertical-align: top;
+}
+
+.el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+</style>
